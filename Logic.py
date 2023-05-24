d2e767c3407c28d16cf6e64eeb30304a7b137d84
spydi2000 <x_men-hero@live.fr>
2023-05-24 23:13:14 +0200

    Ajout Seance
    Ajout du séléctionneur de séances

--- Logic.py
+++ Logic.py
@@ -4,6 +4,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 import os
+import sys
+
 acc,_ = loadUiType(os.path.join('mainwindow.ui'))
 sea,_ = loadUiType(os.path.join('seance.ui'))
 exo,_ = loadUiType(os.path.join('Exercices.ui'))
@@ -38,6 +40,47 @@ class Seance(QWidget,sea):
         super(Seance, self).__init__(parent)
         self.setupUi(self)
 
+        if not createConnection():
+            sys.exit(1)
+
+        self.tableModel = QSqlTableModel(self)
+        self.columnModel=QStandardItemModel()
+
+        self.tableModel.setTable("Seances")
+        self.tableModel.select()
+
+        self.selectData()
+
+        self.columnView.setModel(self.columnModel)
+
+
+    #Fonction qui récupère les données correspondantes aux séances
+    def selectData(self):
+        self.lstSeance = []
+        self.nomExo = []
+        self.lstExo=[]
+        #self.
+        for i in range(self.tableModel.rowCount()) :
+
+            self.data = self.tableModel.record(i)
+
+
+            self.lstSeance.append(QStandardItem(self.data.value("Nom")))
+            self.exo=self.data.value("Liste_exo")
+
+            self.nomExo=self.exo.split(",")
+
+            for j in self.nomExo :
+                self.lstExo.append(QStandardItem(j))
+                self.lstSeance[i].appendRow(self.lstExo[-1])
+
+            self.columnModel.appendRow(self.lstSeance[i])
+
+
+            print(type(self.data.value("Liste_exo")))
+
+    def triExo(self,nomExercice):
+        pass
 
 
 #Classe qui définit la fenêtre des Exercices
@@ -66,13 +109,44 @@ class Exercice(QWidget,exo):
         self.model.setHeaderData(6, Qt.Horizontal, "Vitesse Beta")
         self.model.setHeaderData(7, Qt.Horizontal, "Vitesse Gamma")
 
+
+        #Ajout Limite Movement max positive + negative + valeur que le medecin peut appliquer
+        #Ajout de cycle + durée cycle en fct de durée
+
         #Chargement des données
         self.model.select()
         #On applique le modèle à notre tableau
         self.tableView.setModel(self.model)
+        self.rowToDelete=None
         self.tableView.resizeColumnsToContents()#Sert à redimensionner les colonnes
+        self.tableView.pressed.connect(self.selectDel)
+        self.buttEnrModif.clicked.connect(self.model.submitAll)#Enregistre les modifs
+        self.buttAnnul.clicked.connect(self.model.revertAll)#Annule les modifs
+        self.buttAjtExo.clicked.connect(self.model.insertRow)
+        self.buttDelExo.clicked.connect(self.delWarning)
 
-        self.buttEnrModif.clicked.connect(self.model.submitAll)
+
+#Selectionne la ligne à supprimer
+    def selectDel(self,index):
+        self.ind=index
+        self.rowToDelete=self.ind.row()
+
+#Fonction qui supprimme la ligne
+    def delRowOnClick(self):
+        if self.rowToDelete is not None :
+            self.model.removeRow(self.rowToDelete)
+            self.model.submitAll()
+            self.rowToDelete=None
+
+    #Message de confirmation de suppression
+    def delWarning(self):
+        self.msg = QMessageBox()
+        self.msg.setWindowTitle("Confirmer la suppression")
+        self.msg.setText("Voulez vous vraiment supprimer cette Exercice ?")
+        self.msg.setStandardButtons(QMessageBox.Yes| QMessageBox.Cancel)
+        self.do =self.msg.exec_()
+        if self.do == QMessageBox.Yes :
+            self.delRowOnClick()
 
 
 #Fonction qui crée la connection à notre base de données
